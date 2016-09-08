2e9923f5e2e881bcb59defca0eef9d0d81ce5fed
munxar <munxar@gmx.de>
2016-09-08 11:44:51 +0200

    merged with upstream, removed semicolons;

--- problem72.js
+++ problem72.js
@@ -16,33 +16,30 @@ const calc = () => {
     return out
 }
 
-// TODO fix the output
-function calcFaster() {
+
+const calcFaster = () => {
     // by using native arrays, the memory is allocated continuously and index lookup is much faster
-    const phi = new Int32Array(max + 1);
-    let out = 0;
-    let i, j, phi_i, fac;
+    const phi = new Int32Array(max + 1)
+    let out = 0
+    let i, j, fac
 
     // use for loops, their usually faster than .map
     for (i = 0; i <= max; i++) {
-        phi[i] = i;
+        phi[i] = i
     }
 
     for (i = 2; i <= max; i++) {
-        // reduce lookups
-        phi_i = phi[i];
         if (phi[i] == i) {
             // precalculate factor outside inner loop, division is the most expensive operation
-            fac = (i - 1) / i;
+            fac = (i - 1) / i
             for (j = i; j <= max; j += i) {
-                phi[j] = phi[j] * fac;
+                phi[j] = phi[j] * fac
             }
         }
-        out += phi_i;
+        out += phi[i]
     }
     return out
 }
 
-
-console.log(calc())
+console.log(calcFaster())
 
