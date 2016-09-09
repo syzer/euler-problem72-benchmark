77351a762386559b796f5e7d371bcb22c3c85c17
Gilles Crettenand <gilles.crettenand@liip.ch>
2016-09-09 22:59:33 +0200

    python2 & 3 compat

--- optimizedProblem72.py
+++ optimizedProblem72.py
@@ -1,12 +1,19 @@
+from __future__ import division
+
+try:
+    xrange
+except NameError:
+    xrange = range
+
 def calc():
     limit = 1000000
-    phi = list(range(0, limit + 1))
+    phi = list(xrange(0, limit + 1))
 
     out = 0
-    for x in range(2, limit):
+    for x in xrange(2, limit):
         if phi[x] == x:
             fac = (x - 1) / x
-            for y in range(x, limit, x):
+            for y in xrange(x, limit, x):
                 phi[y] = phi[y] * fac
 
         out += phi[x]
