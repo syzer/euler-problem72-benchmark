36e70b3d28250b557efda95ad2522fb010ed6611
Gilles Crettenand <gilles.crettenand@liip.ch>
2016-09-09 22:38:34 +0200

    use range instead of while

--- optimizedProblem72.py
+++ optimizedProblem72.py
@@ -5,11 +5,9 @@ def calc():
     out = 0
     for x in range(2, limit):
         if phi[x] == x:
-            y = x
             fac = (x - 1) / x
-            while y < limit:
+            for y in range(x, limit, x):
                 phi[y] = phi[y] * fac
-                y += x
 
         out += phi[x]
 
